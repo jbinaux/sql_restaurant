9129dc577312e19efed61fda82e2ff0df8ba6cee
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-08-09 17:31:45 +0200

    insert restaurants

---
+++ insert_restaurants.sql
@@ -0,0 +1,7 @@
+INSERT INTO `restaurant`.`restaurant`
+VALUES
+('taj mahal', 'indienne', 5),
+('sakura', 'japonaise', 4),
+('baguette', 'fancaise', 4),
+('la grande muraille', 'chinoise', 7),
+('mcdo', 'americaine', 5);
